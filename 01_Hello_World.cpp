5313f6fd3e3b3ff7e5c9fb1698e6fd7e7aa1ea9a
asifkhurram <k.asif@bathspa.ac.uk>
2025-11-20 14:53:27 +0000

    First Manual Commit for Demo

---
+++ 01_Hello_World.cpp
@@ -0,0 +1,18 @@
+
+//Update 10/11/2005-13:28
+//the Input/Output Stream library
+#include <iostream>
+
+//Tells the compiler to use the std namespace by default.
+//Without this line, you’d have to write std::cout and std::endl
+// instead of just cout and endl.
+using namespace std;
+
+//main function, the entry point of every C++ program
+int main() {
+
+    //prints "Hello World", followed by a newline (endl).
+    cout << "Hello World3" << endl;
+}
+
+
